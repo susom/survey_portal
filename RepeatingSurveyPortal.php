c5f100c70dcaaa8d80173ea8dfb2e0d3e81bed5e
Andrew Martin <andy123@stanford.edu>
2018-12-19 08:20:50 -0800

    initial version

---
+++ RepeatingSurveyPortal.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Stanford\RepeatingSurveyPortal;
+
+
+class RepeatingSurveyPortal
+{
+
+
+    /**
+     *
+     *
+
+    "redcap_every_page_before_render",
+    "redcap_module_system_enable",
+    "redcap_module_link_check_display",
+    "redcap_module_save_configuration"
+
+     */
+}
