645ce9b95e610570f28999a24d1df27f9862a788
Jae Lee <jael@stanford.edu>
2019-01-23 13:09:54 -0800

    add object to handle incoming text replies

--- web/portal.php
+++ web/portal.php
@@ -24,15 +24,46 @@ How do we handle the content of hte landing page?
 
 
 
- * // METHOD
- * verify hash anbd personal url for record (called by save_record hook)
+ * // METHOD:   x()
+ * verify hash and personal url for record (called by save_record hook)
+ *
+ *
 
+ * // METHOD:  xx ($hash)
+ * retrieve record based on hash or record+config
+ *
 
+ * //METHOD:    xxx ($record, $config)
+ * calculate day number by retrieving start date and calculating against current date
+ *
+
+ * //METHOD:   xxxx($record, $config, $daynumber)
+ * validate window (time) for current time
+ *
+
+ * 1. retrieve record for this hash
+ *
+ *
  * If Autostart:
- *  - if day number
+ *
+  *
+ *  1) if day number is not set, then calculate day number from start date nad current date
+ *
+ *      IF day number is present, then use that day number
+ *
+ * 2) Validate current time window for daynumber
+ *
+ * 3) Create survey for date and redirect
+ *
+ * If Not Autostart, then Calendar (//todo: is there another option?
+ *
+ * 1) Configure all teh Calendar options
+ *    (a) show-missing-day-buttons
+ *    (b)
+ *
+ * 2) if date is selected, validate time window, create survey, redirect
  *
  *
- * // IF day number is present, then start that day
  *
  *
 
