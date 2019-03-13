b9f8cca9804af9ae04a6426098818762ff52d946
Jae Lee <jael@stanford.edu>
2019-03-13 11:46:01 -0700

    fix wrong offset in forecast table

--- src/forecast.php
+++ src/forecast.php
@@ -238,7 +238,7 @@ function renderSummaryTableRows($row_data, $date_window) {
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
 
-    <style><?php echo $module->dumpResource('css/forecast.css'); ?></style>
+    <style><?php echo $module->dumpResource('css/Forecast.css'); ?></style>
 
 
 
