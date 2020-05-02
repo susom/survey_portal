1ba085917c0f973f3478d3a79400d114f317d8e3
Andrew Martin <andy123@stanford.edu>
2020-05-01 17:52:00 -0700

    debuging portal config

--- src/PortalConfig.php
+++ src/PortalConfig.php
@@ -174,6 +174,8 @@ class PortalConfig {
         $this->mainConfigEventName = REDCap::getEventNames(true, false, $this->mainConfigEventID);
         $this->surveyEventName = REDCap::getEventNames(true, false, $this->surveyEventID);
 
+        $module->emDebug($this);
+
     }
 
 
